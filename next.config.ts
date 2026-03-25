7aa34c265cab80b7035faf5448332bfd7d215e90
nathalia-dssb <nath.dsbb@gmail.com>
2026-03-25 10:18:26 -0600

    fix: pages config

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import { withNextVideo } from "next-video/process";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: "/cumbre-bachilleres",
-  assetPrefix: "/cumbre-bachilleres/",
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "/cumbre-bachilleres",
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH ? `${process.env.NEXT_PUBLIC_BASE_PATH}/` : "/cumbre-bachilleres/",
 
   images: {
     unoptimized: true,
